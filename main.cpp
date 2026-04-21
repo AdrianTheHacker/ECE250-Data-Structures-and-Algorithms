c2a5c64db371ddaa56439ede429c2a2465f12f1d
Adrian Tarantino <57376726+AdrianTheHacker@users.noreply.github.com>
2026-04-21 16:10:29 -0400

    Implementing merge sort

---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+void print(int arr[], int size);
+
+// Merge Sort
+void merge_sort(int arr[], int size);
+void merge(int arr[], int pivot, int size);
+
+int main() {
+    int numbers[] = {2, 9, 5, 1, 0, 7, 6};
+    int size = 7;
+
+    print(numbers, size);
+    merge_sort(numbers, size);
+    print(numbers, size);
+
+    return 0;
+}
+
+void print(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i];
+        if (i < size - 1) std::cout << ", ";
+    }
+    std::cout << std::endl;
+}
+
+////////////////
+// Merge Sort //
+////////////////
+void merge_sort(int arr[], int size) {
+    if(size > 2) {
+        int pivot = size / 2;
+        int new_size = size - pivot;
+
+        merge_sort(&arr[0], pivot);
+        merge_sort(&arr[pivot], new_size);
+
+        merge(arr, pivot, size);
+    }
+
+    else {
+        if(size == 2) {
+            if(arr[0] > arr[1]) {
+                int temp = arr[0];
+                arr[0] = arr[1];
+                arr[1] = temp;
+            }
+        }
+    }
+}
+
+void merge(int arr[], int pivot, int size) {
+    int *new_arr = new int[size];
+
+    int i = 0;
+    int j = pivot;
+
+    for(int k = 0; k < size; k++) {
+        if(i == pivot) {
+            new_arr[k] = arr[j];
+            j++;
+            continue;
+        }
+
+        if(j == size) {
+            new_arr[k] = arr[i];
+            i++;
+            continue;
+        }
+
+        if(arr[i] < arr[j]) {
+            new_arr[k] = arr[i];
+            i++;
+            continue;
+        }
+
+        new_arr[k] = arr[j];
+        j++;
+    }
+
+    for(int k = 0; k < size; k++) {
+        arr[k] = new_arr[k];
+    }
+
+    delete[] new_arr;
+    new_arr = nullptr;
+}
